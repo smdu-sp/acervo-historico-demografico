68a3ab120f62461bdd23af9c5af1111504875007
vgiuliani11 <vinicius.giuliani@gmail.com>
2026-04-24 12:57:59 -0300

    feat: utiliza o emulador Ruffle para exibição do conteúdo Flash

--- historico_demografico/animacao.php
+++ historico_demografico/animacao.php
@@ -1,12 +1,47 @@
 <?php include("cabecalho.php"); ?>
 </div>
 <div id="animacao">
-<noscript><object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0" width="100%" height="300"> 
-  <param name="movie" value="img/animacao.swf">
-  <param name="quality" value="high">
-  <embed src="img/animacao.swf" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type=application/x-shockwave-flash" width="100%" height="300"></embed>
-</object>
-</noscript>
+  <script language="JavaScript1.2" src="funcoes.js"></script>
+
+  <script>
+    window.RufflePlayer = window.RufflePlayer || {};
+    window.RufflePlayer.config = {
+      wmode: "transparent",
+      backgroundColor: null
+    };
+  </script>
+
+  <script src="https://unpkg.com/@ruffle-rs/ruffle"></script>
+
+  <embed src="img/animacao.swf"
+    type="application/x-shockwave-flash"
+    wmode="transparent"
+    width="100%"
+    height="300">
+
+  <noscript>
+    <style>
+      .aviso-noscript {
+        background-color: #FDF5E6;
+        border-left: 5px solid #FF6600;
+        color: #333333;
+        font-family: 'Trebuchet MS', Arial, Helvetica, sans-serif;
+        font-size: 14px;
+        padding: 15px 20px;
+        margin: 20px auto;
+        width: 90%;
+        max-width: 450px;
+        text-align: center;
+        border-radius: 0 4px 4px 0;
+        box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+      }
+    </style>
+
+    <div class="aviso-noscript">
+      <b>O JavaScript está desabilitado.</b><br><br>
+      Por favor, ative o JavaScript nas configurações do seu navegador para conseguir visualizar a animação.
+    </div>
+  </noscript>
 </div>
 <div>
 <?php include("rodape.php"); ?>
